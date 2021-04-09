16a62eb1013de715c3d4f047fe21cc4210cb39ae
aconaghan <Aisling.Conaghan@irishlife.ie>
2021-04-09 18:49:09 +0100

    Fourth commit

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ print(col_list)
 #tidying up data types
 print(df.dtypes)
 df['Date of Sale (dd/mm/yyyy)'] = pd.to_datetime(df['Date of Sale (dd/mm/yyyy)'])
+print(df['Price (€)'].apply(type).value_counts())
+df['Price (€)'] = df['Price (€)'].str[1:]
+df['Price (€)'] = df['Price (€)'].str.replace(',','').astype('float')
 print(df.dtypes)
 df['Year'] = pd.DatetimeIndex(df['Date of Sale (dd/mm/yyyy)']).year
 
@@ -25,6 +28,11 @@ print(df_no_dup.shape)
 #checking for missing values
 print(df_no_dup.isnull().sum())
 df_no_dup = df_no_dup.fillna(0)
+
+#count of irish entries in 'Description of Property'
+print(df_no_dup['Description of Property'].value_counts())
+df_no_dup = df_no_dup[df_no_dup['Description of Property'].isin(['Second-Hand Dwelling house /Apartment', 'New Dwelling house /Apartment'])]
+print(df_no_dup.shape)
 
 #dictionary for mapping county to region
 map_region = { 'Cavan':'Border',
@@ -57,7 +65,7 @@ map_region = { 'Cavan':'Border',
 #mapping county to region
 df_no_dup['Region'] = df_no_dup['County'].map(map_region)
 
-#adding a flag for Dublin
+#adding a flag for Dublin using looping
 result = []
 for value in df_no_dup["Region"]:
     if value == "Dublin":
@@ -69,10 +77,17 @@ df_no_dup["Dublin_Flag"] = result
 
 #merging on pop density
 pop_data = pd.read_excel(r'pop_density.xlsx', skiprows=[0,1])
-print(pop_data)
+print(pop_data.dtypes)
+cols = pop_data.select_dtypes(['object']).columns
+pop_data[cols] = pop_data[cols].apply(lambda x: x.str.strip())
 
 df2 = df_no_dup.merge(pop_data, on='Region', how='left')
-print(df2[df2['Region'] == "Dublin"])
+print(df2.isnull().sum())
+print(df2.shape)
+print(df2.columns)
+
+#grouped summary statistics
+#avg_price = df2.groupby(['Region', 'Description of Property'])['Price (€)'].mean()
 
 
 
