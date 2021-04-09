054d5315fcde68e93588e503da2b39c9d63ca9b4
aconaghan <Aisling.Conaghan@irishlife.ie>
2021-04-09 12:59:09 +0100

    Third commit

--- main.py
+++ main.py
@@ -54,6 +54,29 @@ map_region = { 'Cavan':'Border',
                'Mayo':'West',
                'Roscommon':'West' }
 
+#mapping county to region
+df_no_dup['Region'] = df_no_dup['County'].map(map_region)
+
+#adding a flag for Dublin
+result = []
+for value in df_no_dup["Region"]:
+    if value == "Dublin":
+        result.append(1)
+    else:
+        result.append(0)
+
+df_no_dup["Dublin_Flag"] = result
+
+#merging on pop density
+pop_data = pd.read_excel(r'pop_density.xlsx', skiprows=[0,1])
+print(pop_data)
+
+df2 = df_no_dup.merge(pop_data, on='Region', how='left')
+print(df2[df2['Region'] == "Dublin"])
+
+
+
+
 
 
 
